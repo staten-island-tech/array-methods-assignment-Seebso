86f1c166aa06100a2d16a68e2db1b9835a375d7f
Seebso <124058253+Seebso@users.noreply.github.com>
2023-10-06 13:12:44 -0400

    das

---
+++ app.js
@@ -0,0 +1,18 @@
+const HSR = [
+
+{   Name: "Caelus",
+    Element: "Vary",
+    Path: "Vary",
+    Rating: "5",
+    Affiliation: "Astral Express"
+},
+{
+    Name: "Stelle",
+    Element: "Vary",
+    Path: "Vary",
+    Rating: "5",
+    Affiliation: "Astral Express"
+},
+];
+// arrow function ()=> auto returns output, this keyword points global.
+HSR.forEach((Name)=> console.log(HSR.Name));
