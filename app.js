819e905298e46df7b016acbba75b1b8741aebd6b
Seebso <124058253+Seebso@users.noreply.github.com>
2023-10-17 13:12:54 -0400

    sdffa

--- app.js
+++ app.js
@@ -4,28 +4,42 @@ const HSR = [
     Element: "Vary",
     Path: "Vary",
     Rating: "5",
-    Affiliation: "Astral Express"
+    Affiliation: "Astral Express",
+    WorldStoryAppear: ['1', '2', '3']
 },
 {
     Name: "Stelle",
     Element: "Vary",
     Path: "Vary",
     Rating: "5",
-    Affiliation: "Astral Express"
+    Affiliation: "Astral Express",
+    WorldStoryAppear: ['1', '2', '3']
 },
 {
     Name: "March 7th",
     Element: "Ice",
     Path: "Preservation",
     Rating: "4",
-    Affiliation: "Astral Express"
+    Affiliation: "Astral Express",
+    WorldStoryAppear: ['1', '2', '3']
 },
 
 ];
 // arrow function ()=> auto returns output, this keyword points global.
-HSR.forEach((HSR) => console.log(HSR.Name));
 /* const graduated = students.filter((student) => student.graduated === true);
 console.log(graduated); */
-Name
-    .filter((HSR)=> Rating>4) 
-    .forEach((Name)=> console.log(Rating));
+    const Rating = HSR.filter((HSR) => HSR.Rating>4);
+    console.log(Rating)
+HSR.forEach((HSR) => console.log(HSR.Name));
+HSR.forEach((HSR) => console.log(HSR.Element));
+HSR.forEach((HSR) => console.log(HSR.Path));
+HSR.forEach((HSR) => console.log(HSR.Rating));
+HSR.forEach((HSR) => console.log(HSR.Affiliation));
+HSR.forEach((HSR) => {
+    HSR.WorldStoryAppear.forEach((WorldStory) => {
+        console.log(WorldStory)
+    })})
+    
+    
+
+
